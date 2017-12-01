8f2c9283c6d1a343b73e285d45f7cdd17de53b17
Jenna Ritten <jritten@umich.edu>
2017-12-01 14:33:40 -0800

    Config for Rails API mode.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module BootUp
   class Application < Rails::Application
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
