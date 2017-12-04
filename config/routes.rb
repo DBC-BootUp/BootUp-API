b6110b2b51326b26e92dcd3e1acaf751b6729635
Jenna Ritten <jritten@umich.edu>
2017-12-04 08:29:53 -0800

    Added routes for 'users', companies', and 'interviews' to routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   scope :api do
+    root to: 'interviews#index'
 
+    resources :users
+    resources :sessions, only: [:new, :create, :delete]
+
+    resources :companies do 
+      resources :interviews
+    end
+
+    resources :users do 
+      resources :interviews
+    end
   end
 end
