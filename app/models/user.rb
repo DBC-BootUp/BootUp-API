266160b7a9921ef27aa34465e9cf17df69a8e29a
Josh Leichtung <joshleichtung@gmail.com>
2017-12-04 07:18:17 -0800

    Update associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :skills
-  has_many :interviews
+  has_many :interviews, dependent: :destroy
+  has_many :companies_applied_to, through: :interviews, source: :company
 end
