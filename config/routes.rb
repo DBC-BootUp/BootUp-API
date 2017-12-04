fb051b0f5749ef5ee6057e09644db3536bcf6e5d
Jenna Ritten <jritten@umich.edu>
2017-12-04 10:56:20 -0800

    correction.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,8 @@
 Rails.application.routes.draw do
   scope :api do
-    root to: 'interviews#index'
-
     resources :users
-    resources :sessions, only: [:new, :create, :delete]
-
-    resources :companies do 
-      resources :skills, only: [:index, :new, :create]
-      resources :interviews
-    end
-
-    resources :users do 
-      resources :skills, only: [:index, :new, :create]
-      resources :interviews
-    end
-
-    resources :interviews do
-      resources :skills, only: [:index, :new, :create]
-    end
+    resources :interviews
+    resources :companies
+    resources :skills, only: [:create, :index]
   end
 end
