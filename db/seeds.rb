cea6feb3e08613ec8529f4a7696b234f95c90302
Josh Leichtung <joshleichtung@gmail.com>
2017-12-04 07:23:58 -0800

    Add factories and seed file for initial models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Skill.destroy_all
+User.destroy_all
+Company.destroy_all
+Interview.destroy_all
+
+FactoryBot.create_list(:skill, 30)
+FactoryBot.create_list(:user, 5)
+FactoryBot.create_list(:company, 10)
+FactoryBot.create_list(:interview, 50)
