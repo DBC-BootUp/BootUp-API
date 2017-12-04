2c8ce75b256e309797fb98995627d9bb9349a0db
Josh Leichtung <joshleichtung@gmail.com>
2017-12-04 09:12:25 -0800

    Add json_with_association methods to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ApplicationRecord
   has_and_belongs_to_many :skills
   has_many :interviews, dependent: :destroy
   has_many :companies_applied_to, through: :interviews, source: :company
+
+  def json_with_associations
+    self.as_json(include: { 
+      skills: {only: [:id, :name]},
+      interviews: {include: :company},
+      companies_applied_to: {}
+    })
+  end
+
+  def serializable_hash(options = nil)
+    super({except: [:created_at, :updated_at]}.merge(options || {}))
+  end
 end
