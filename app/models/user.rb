60574fba8c69b164738efbce7482ab69dd0af9f6
Josh Leichtung <joshleichtung@gmail.com>
2017-12-06 13:55:07 -0800

    Add linked in user fields to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,19 @@ class User < ApplicationRecord
   def serializable_hash(options = nil)
     super({except: [:created_at, :updated_at]}.merge(options || {}))
   end
+
+  def self.find_or_create_from_auth_hash(auth_hash)
+    self.find_or_create_by(uid: auth_hash[:uid]) do |user|
+      user.name = auth_hash[:info][:first_name] + ' ' + auth_hash[:info][:last_name]
+      user.email = auth_hash[:info][:email]
+      # user.description = auth_hash[:info][:description]
+      user.location = auth_hash[:info][:location]
+      user.photo_url = auth_hash.extra.raw_info.pictureUrls.values[1][0]
+      user.linkedin_url = auth_hash[:info][:urls][:public_profile]
+      # user.linkedin_token = auth_hash[:credentials][:token]
+      # user.linkedin_token_expiration = auth_hash[:credentials][:expires_at]
+      user.current_company = auth_hash[:extra][:raw_info][:positions][:values][0][:company][:name]
+      user.current_position = auth_hash[:extra][:raw_info][:positions][:values][0][:title]
+    end
+  end
 end
