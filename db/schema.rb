e944838ce2c3216bd39245d0fc2b91458b7a0c0a
Josh Leichtung <joshleichtung@gmail.com>
2017-12-06 21:41:37 -0800

    Add completed registration flag to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205193026) do
+ActiveRecord::Schema.define(version: 20171207054024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20171205193026) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "uid"
+    t.boolean "completed_registration"
   end
 
 end
